0fdcf4f1dbbc86e78694fe99215f97deb368ba61
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-07-06 20:03:38 +0530

    hi

--- app/src/main/java/read/code/yourreader/activities/LoginActivity.kt
+++ app/src/main/java/read/code/yourreader/activities/LoginActivity.kt
@@ -41,7 +41,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+//oh yeah
         init()
 
         home_no_account_log.setOnClickListener {
