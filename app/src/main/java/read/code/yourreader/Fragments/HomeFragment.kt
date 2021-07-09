e8f68cae0f1e331d1f1cc8510918d1fa028ef058
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-07-09 19:52:47 +0530

    manifest changed for accepting pdf and text file code in Home Fragment

--- app/src/main/java/read/code/yourreader/Fragments/HomeFragment.kt
+++ app/src/main/java/read/code/yourreader/Fragments/HomeFragment.kt
@@ -1,10 +1,13 @@
 package read.code.yourreader.Fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import read.code.yourreader.R
 
 
@@ -23,7 +26,36 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         var view= inflater.inflate(R.layout.fragment_home, container, false)
 
+        val intent = requireActivity().intent
+        if (intent != null) {
+            val action = intent.action
+            val type = intent.type
+            if (Intent.ACTION_SEND == action && type != null) {
+                if (type.equals("text/plain", ignoreCase = true)) {
+                    handleTextData(intent)
+                }
+                else if (type.equals("application/pdf", ignoreCase = true)) {
+                    handlePdfFile(intent)
+                }
+            }
+        }
+
         return view
+    }
+
+    private fun handlePdfFile(intent: Intent) {
+        val pdffile: Uri? = intent.getParcelableExtra(Intent.EXTRA_STREAM)
+        if (pdffile != null) {
+            Log.d("Pdf File Path : ", "" + pdffile.path)
+        }
+    }
+
+    private fun handleTextData(intent: Intent) {
+        val textdata = intent.getStringExtra(Intent.EXTRA_TEXT)
+        if (textdata != null) {
+            Log.d("Text Data : ", "" + textdata)
+            //downloadFile(textdata)
+        }
     }
 
 }
