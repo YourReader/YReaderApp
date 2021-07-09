e50946ed553d26e30656f32b882d1d57d42a8643
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-07-09 10:59:25 +0530

    Permission Added in DownloadsFragment.kt

--- app/src/main/java/read/code/yourreader/Fragments/DownloadsFragment.kt
+++ app/src/main/java/read/code/yourreader/Fragments/DownloadsFragment.kt
@@ -1,12 +1,12 @@
 package read.code.yourreader.Fragments
 
-import android.Manifest
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.app.Activity
 import android.app.AlertDialog
+import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -38,29 +38,29 @@ class DownloadsFragment : Fragment() {
     }
 
 
-    private fun checkPermissions(permission:String,name:String,requestCode:Int){
+    private fun checkPermissions(permissions:String, permission_name:String, code:Int){
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
             when{
-                 ContextCompat.checkSelfPermission(requireContext(),permission)  == PackageManager.PERMISSION_GRANTED ->{
-                    Toast.makeText(requireContext(), "Permission Granted", Toast.LENGTH_SHORT).show()
+                 ContextCompat.checkSelfPermission(requireContext(),permissions)  == PackageManager.PERMISSION_GRANTED ->{
+                     Log.d(TAG, "checkPermissions: Permission Granted")
                 }
-                shouldShowRequestPermissionRationale(permission) -> showDialog(permission,name,requestCode)
+                shouldShowRequestPermissionRationale(permissions) -> showDialog(permissions,permission_name,code)
 
-                else -> ActivityCompat.requestPermissions(requireActivity(), arrayOf(permission),requestCode)
+                else -> ActivityCompat.requestPermissions(requireActivity(), arrayOf(permissions),code)
             }
         }
     }
 
     private fun showDialog(permission: String, name: String, requestCode: Int) {
-        val builder=AlertDialog.Builder(requireView().context)
+        val builder=AlertDialog.Builder(requireContext())
 
         builder.apply {
             setMessage("Permission to Access Your Pdf and Readable Documents")
             setTitle("Permission Required")
             setPositiveButton("Ok"){
-                dialog,which->
+                    _, _ ->
 
-                ActivityCompat.requestPermissions(requireView().context as Activity, arrayOf(permission),requestCode)
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(permission),requestCode)
 
             }
         }
