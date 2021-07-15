8fa3a77bd259cdf7a9bee7d90bb36cf4b9155a27
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-07-15 18:50:44 +0530

    settings2.xml layout Done

--- app/src/main/java/read/code/yourreader/Fragments/SettingsFragment.kt
+++ app/src/main/java/read/code/yourreader/Fragments/SettingsFragment.kt
@@ -1,19 +1,22 @@
 package read.code.yourreader.Fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+
+
+import androidx.preference.PreferenceFragmentCompat
 import read.code.yourreader.R
 
-class SettingsFragment : Fragment() {
-    override fun onCreateView( //the fragment is initialized and bound to the nav host activity.
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+class SettingsFragment :  PreferenceFragmentCompat() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.settings2)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
     }
 }
 
