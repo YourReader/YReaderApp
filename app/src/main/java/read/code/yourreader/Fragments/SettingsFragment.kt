31b4507e473b148466065acc023ae4859afabd25
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-07-15 21:07:09 +0530

    settings2.xml layout Done

--- app/src/main/java/read/code/yourreader/Fragments/SettingsFragment.kt
+++ app/src/main/java/read/code/yourreader/Fragments/SettingsFragment.kt
@@ -2,12 +2,16 @@ package read.code.yourreader.Fragments
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.preference.Preference
 
 
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import read.code.yourreader.R
 
-class SettingsFragment :  PreferenceFragmentCompat() {
+class SettingsFragment :  PreferenceFragmentCompat()  {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +20,24 @@ class SettingsFragment :  PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        val sp=PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        val swipeBrightNess=sp.getBoolean("BrightnessBySwipe",true)
+
+        val darkMode=sp.getBoolean("DarkMode",false)
+        val screenOn=sp.getBoolean("ScreenOn",false)
+        val AccessDocs=sp.getBoolean("AccessDocs",false)
+
+        val upgradeToPremium=sp.getString("Upgrade","")
+
+
+        Log.d("TAG", "onCreatePreferences: $swipeBrightNess \n $darkMode " +
+                "\n $screenOn \n $AccessDocs \n $upgradeToPremium \n ")
+
+
 
     }
+
+
 }
 
