e416240db14f8b04c5b4b821f5f2b7bafab9aa10
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-07-17 11:21:25 +0530

    Google btn Fixed Fonts Fixed SettingsFragment.kt changed

--- app/src/main/java/read/code/yourreader/Fragments/SettingsFragment.kt
+++ app/src/main/java/read/code/yourreader/Fragments/SettingsFragment.kt
@@ -5,12 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
 import read.code.yourreader.R
-
-
+import read.code.yourreader.databinding.FragmentHomeBinding
+import read.code.yourreader.databinding.FragmentSettingsBinding
+import read.code.yourreader.di.components.DaggerFactoryComponent
+import read.code.yourreader.di.modules.FactoryModule
+import read.code.yourreader.di.modules.RepositoryModule
+import read.code.yourreader.mvvm.repository.MainRepository
+import read.code.yourreader.mvvm.viewmodels.MainViewModel
 
 
 class SettingsFragment : Fragment() {
+
+    lateinit var binding: FragmentSettingsBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var component: DaggerFactoryComponent
+    private lateinit var mAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +33,24 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+
+        mAuth= FirebaseAuth.getInstance()
+        component = DaggerFactoryComponent.builder()
+            .repositoryModule(RepositoryModule(requireContext()))
+            .factoryModule(FactoryModule(MainRepository(requireContext())))
+            .build() as DaggerFactoryComponent
+        viewModel = ViewModelProviders.of(this, component.getFactory())
+            .get(MainViewModel::class.java)
 
 
-        return view
+        binding.SignOutSettings.setOnClickListener {
+            viewModel.signOut()
+            requireActivity().finish()
+        }
+
+        return binding.root
     }
 
 
