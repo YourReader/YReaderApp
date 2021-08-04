3cddb3e7fa92f400a9e97ba33d24e62ce2172197
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-08-04 14:25:22 +0530

    card pdf click done

---
+++ app/src/androidTest/java/read/code/yourreader/mvvm/repository/HomeRepository.kt
@@ -0,0 +1,8 @@
+package read.code.yourreader.mvvm.repository
+
+import android.content.Context
+
+
+class HomeRepository(var context: Context) : BaseRepository(context) {
+
+}
--- app/src/main/java/read/code/yourreader/MVVVM/repository/MainRepository.kt
+++ app/src/main/java/read/code/yourreader/MVVVM/repository/MainRepository.kt
@@ -1,9 +1,20 @@
 package read.code.yourreader.mvvm.repository
 
+import android.content.ContentValues
 import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.itextpdf.text.pdf.PdfReader
+import com.itextpdf.text.pdf.parser.LocationTextExtractionStrategy
+import com.itextpdf.text.pdf.parser.PdfTextExtractor
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStream
 
-class MainRepository(context: Context) : BaseRepository(context) {
+class MainRepository(var context: Context) : BaseRepository(context) {
     private var pdfs = ArrayList<File>()
 
     fun searchPdf(dir: File): ArrayList<File> {
@@ -21,5 +32,37 @@ class MainRepository(context: Context) : BaseRepository(context) {
             }
         }
         return pdfs
+    }
+
+    fun extractTextFromPdfFile(toUri: Uri): java.util.ArrayList<String> {
+        var pages = 0
+        lateinit var inputStream: InputStream
+
+        var builderArray = ArrayList<String>()
+        var fileContent: String
+        val reader: PdfReader?
+        try {
+            inputStream = context.contentResolver.openInputStream(toUri)!!
+            reader = PdfReader(inputStream)
+            pages = reader.numberOfPages
+            for (i in 1..pages) {
+                fileContent =
+                    PdfTextExtractor.getTextFromPage(
+                        reader,
+                        i,
+                        LocationTextExtractionStrategy()
+                    )
+                builderArray.add(fileContent)
+            }
+            reader.close()
+
+
+        } catch (e: IOException) {
+            Log.d(ContentValues.TAG, "extractTextFromPdfFile: ${e.message}")
+        } catch (e: FileNotFoundException) {
+            Toast.makeText(context, "File Not Found", Toast.LENGTH_SHORT).show()
+        }
+
+        return builderArray
     }
 }
